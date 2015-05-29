be11dc7973bc7fb1ff5f700c45c1eec77368a721
Khwilo Kabaka <kwhyzzy@gmail.com>
2015-05-29 14:16:24 +0300

    check to see if Google Maps is able to load in the application

--- app/src/main/java/com/khwilo/location/MainActivity.java
+++ app/src/main/java/com/khwilo/location/MainActivity.java
@@ -1,39 +1,33 @@
 package com.khwilo.location;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends FragmentActivity {
+    GoogleMap map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        if(map == null){
+            Toast.makeText(this, "Google Maps is not available", Toast.LENGTH_LONG).show();
+        }
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+    public boolean onCreateOptionsMenu(Menu menu){
+        //adds items to the action bar
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
 }
